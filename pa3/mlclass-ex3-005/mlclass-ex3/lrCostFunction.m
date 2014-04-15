348e45ef9eb90f732b3ed0336ccd913182c483ec
Mikhail Plekhanov <m.plekhanov@netris.ru>
2014-04-15 18:45:26 +0400

    HW3 solution.

--- pa3/mlclass-ex3-005/mlclass-ex3/lrCostFunction.m
+++ pa3/mlclass-ex3-005/mlclass-ex3/lrCostFunction.m
@@ -36,15 +36,8 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
-
+J = (1.0/m)*sum(-y'*log(sigmoid(X*theta)) - (1 - y')*log(1-sigmoid(X*theta))) + lambda/(2*m)*sum(theta(2:end).^2);
+grad = 1.0/m*(sigmoid(X*theta) - y)'*X + [0 lambda/m*theta(2:end,1)'];
 % =============================================================
 
 grad = grad(:);
